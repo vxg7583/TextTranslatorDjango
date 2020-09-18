ab7dd15d979acb61945cc3f2ceb6474719c73e33
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-09-18 08:37:19 -0700

    small change

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ then
     echo "python there"
     pip install --user virtualenv
     python -m venv env
-    source /env/bin/activate
+    source env/bin/activate
     activate
     pip install -r requirements.txt
-    python manage.py runserver
+    python manage.py runserver 8080
 else 
     echo "Please install python 3+ and source or set it as the environment variable"
 fi
